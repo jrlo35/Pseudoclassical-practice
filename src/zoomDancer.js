2ecdf789beac20b9005be0b5205297ccdf4ae183
Jonathon Lo <jonathon.lo35@gmail.com>
2016-01-18 20:12:45 -0800

    'added all animations'

--- src/zoomDancer.js
+++ src/zoomDancer.js
@@ -3,6 +3,7 @@
 var ZoomDancer = function(top, left, timeBetweenSteps){
   BlinkyDancer.call(this, top, left, timeBetweenSteps);
   this.isZoomed=false;
+  this.$node.addClass("zoom-dancer");
 
 }
 
@@ -14,16 +15,17 @@ ZoomDancer.prototype.step = function(){
   if(!this.isZoomed) {
     this.$node.animate({
         width: "20px",
-        height: "20px"
+        height: "20px",
+        marginLeft: "100px"
     }, this.timeBetweenSteps);
     this.isZoomed = true;
   }
   else {
     this.$node.animate({
         width: "10px",
-        height: "10px"
+        height: "10px",
+        marginLeft: "0px"
     }, this.timeBetweenSteps);
     this.isZoomed = false;
   }
-
-}
+};
