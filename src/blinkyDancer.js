528e15db2688b76da379b25d989d1a57bc1d201f
Jonathon Lo <jonathon.lo35@gmail.com>
2016-01-18 16:31:09 -0800

    'added blinkydancer'

---
+++ src/blinkyDancer.js
@@ -0,0 +1,14 @@
+var BlinkyDancer = function(top, left, timeBetweenSteps){
+  
+  
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+};
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+BlinkyDancer.prototype.step = function(){
+
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  this.$node.toggle();
+
+}
